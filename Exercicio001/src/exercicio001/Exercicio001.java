781387e71ac3b64d1f86a5d94b3fb6905b94d805
Mateus Estevam <mateusevangelistaestevam@gmail.com>
2024-09-18 19:47:14 -0300

    ex001

---
+++ Exercicio001/src/exercicio001/Exercicio001.java
@@ -0,0 +1,9 @@
+package exercicio001;
+
+public class Exercicio001 {
+    public static void main(String[] args){
+        System.out.println("Escreva um programa que mostre na tela a mensagem 'Olá, Mundo!' ");
+        
+        System.out.println("Olá Mundo!!!");
+    }
+}
