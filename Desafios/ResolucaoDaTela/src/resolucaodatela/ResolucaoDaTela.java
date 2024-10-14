0db51df91b58f337567b8da8c57597e6d78230b5
Mateus Estevam <mateusevangelistaestevam@gmail.com>
2024-10-14 18:55:17 -0300

    ResolucaoDeTela

---
+++ Desafios/ResolucaoDaTela/src/resolucaodatela/ResolucaoDaTela.java
@@ -0,0 +1,13 @@
+package resolucaodatela;
+import java.awt.Toolkit;
+import java.awt.Dimension;
+
+public class ResolucaoDaTela {
+    
+    public static void main(String[] args) {
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension dimensao = kit.getScreenSize();
+        System.out.println("A resolução da Tela deste computador é de "+dimensao.width+" de largura por "+dimensao.height+" de altura.");       
+    }
+    
+}
