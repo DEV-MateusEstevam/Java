7a7f567fbb2e27fba8198152ce5ed955fad022bd
Mateus Estevam <mateusevangelistaestevam@gmail.com>
2024-10-22 20:48:16 -0300

    Exercicio 21/10

---
+++ Desafios/Aula2110/src/Exercicio1/Principal.java
@@ -0,0 +1,18 @@
+package Exercicio1;
+
+
+public class Principal {
+    public static void main(String[] args){
+    //Criando um objeto (instancia) da class Veiculo
+    
+        //Criando um objeto através do construtor
+        Veiculo carro01 = new Veiculo();
+        
+        carro01.setMarca("Volkswagem");
+        carro01.setModelo("Fusca");
+        carro01.setCor("Azul");
+        carro01.setAno(2014);
+        
+        carro01.mostrarDados();
+    }
+}
---
+++ Desafios/Aula2110/src/Exercicio1/Veiculo.java
@@ -0,0 +1,52 @@
+package Exercicio1;
+
+public class Veiculo {
+    // definindo os atributos = variaveis
+    public String Marca, Modelo, Cor;
+    public int Ano;
+    
+    //metodos sets(colocar): definindo conteudo
+    public void setMarca(String Marca){
+        this.Marca = Marca;
+    }
+    
+    public void setModelo(String Modelo){
+        this.Modelo = Modelo;
+    }
+    
+    public void setCor(String Cor){
+        this.Cor = Cor;
+    }
+    
+    public void setAno(int Ano){
+        this.Ano = Ano;
+    }
+    
+    //metodo gets: retornar o conteudo recebido
+    public String getMarca(){
+        return Marca;
+    }
+    
+    public String getModelo(){
+        return Modelo;
+    }
+    
+    public String getCor(){
+        return Cor;
+    }
+    
+    public int getAno(){
+        return Ano;
+    }
+    
+    public void mostrarDados(){
+        System.out.println("Marca: "+this.getMarca());
+        System.out.println("Modelo: "+this.getModelo());
+        System.out.println("Cor: "+this.getCor());
+        System.out.println("Ano de Fabricação: "+this.getAno());
+    
+    }
+}
+
+
+
