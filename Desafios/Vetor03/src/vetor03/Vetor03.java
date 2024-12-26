be12f8fd97bd045dfa0f0f4a9ebc9750a72ba48d
Mateus Estevam <mateusevangelistaestevam@gmail.com>
2024-12-26 19:14:32 -0300

    Update Vetor03.java

--- Desafios/Vetor03/src/vetor03/Vetor03.java
+++ Desafios/Vetor03/src/vetor03/Vetor03.java
@@ -9,13 +9,14 @@ public class Vetor03 {
         double numero1[] = {1.2, 4.6, 6.7, 4.5, 2.5, -9.8, 10.5, -0.2};
         //FOR EACH, para cada elemento, utilizado somente com vetores
         Arrays.sort(numero1);//Arrays.sort coloca os valores em ordem dentro do Vetor
+        
         for(double valor : numero1){
             System.out.print(valor+", ");
         }
+        
         System.out.println();
         System.out.println();
         System.out.println("O vetor Numero1 ocupa "+numero1.length+" Casas ao todo!");
         System.out.println();
     }
-    
 }
