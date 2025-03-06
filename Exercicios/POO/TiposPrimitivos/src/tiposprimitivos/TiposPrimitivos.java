81599fbd003ad2c3fd16f68c78e06bf63532414b
Mateus Estevam <mateusevangelistaestevam@gmail.com>
2025-03-06 19:29:33 -0300

    TiposPrimitivos

---
+++ Exercicios/POO/TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,49 @@
+package tiposprimitivos;
+
+import java.util.Scanner;
+
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+        //int idade = 30;
+        //String valor = Integer.toString(idade);//Integer = classe involucro
+        
+        String valor = "59.1";
+        //int idade = Integer.parseInt(valor);
+        //float v1 = Float.parseFloat(valor);
+        double d1 = Double.parseDouble(valor);
+        System.out.println(d1);
+        
+        /* Entrada de dados 
+        Scanner touch = new Scanner(System.in);//System.in = monitora a entrada de dados
+        System.out.print("Digite o nome do(a) Aluno(a):");
+        String nome = touch.nextLine();//nextLine para String
+        System.out.println();
+        System.out.print("Digite a nota do(a) "+nome+":");
+        float nota = touch.nextFloat();//para Float
+        //double nota = touch.nextDouble();//para double
+        
+        System.out.format("A nota do(a) %s é %.1f \n", nome, nota);
+        System.out.println();
+        
+         Tipos primitivos
+        
+        float notaB = 8.5f;
+        float notaB1 = (float) 8.5;
+        double notaA = 9.8;
+        double notaA1 = (double) 9.8;
+        String nome = "mateus";
+        
+        System.out.println("Nota A sem Typecast: "+notaA);
+        System.out.println("Nota A com Typecast: "+notaA1);
+        System.out.println("Nota B sem Typecast: "+notaB);
+        System.out.println("Nota B com Typecast: "+notaB1);
+        //impressão formatada
+        System.out.printf("A nota do(a) %s é %.2f \n", nome, notaA);
+        System.out.format("A nota do(a) %s é %.3f \n", nome, notaB);
+        //System.out.println();
+        */
+        
+    }
+    
+}
